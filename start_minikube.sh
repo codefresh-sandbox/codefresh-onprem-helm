8c6a87df575cd09be021040b2aac900d1d708ec0
Alexei Ledenev <alexei.led@gmail.com>
2017-06-27 16:33:46 +0300

    not sure if it's working

--- start_minikube.sh
+++ start_minikube.sh
@@ -6,4 +6,7 @@ brew install docker-machine-driver-xhyve
 # uncomment to enable RBAC
 # RBAC="--extra-config=apiserver.Authorization.Mode=RBAC"
 
-minikube start --cpus=4 --memory=8192 --vm-driver=xhyve --disk-size=40g $RBAC
+# uncomment to enable development on localhost
+# DEV="--network-plugin=kubenet --extra-config=kubelet.PodCIDR=10.10.0.0/24 --extra-config=kubelet.NonMasqueradeCIDR=10.10.0.0/24"
+
+minikube start --cpus=4 --memory=8192 --vm-driver=xhyve --disk-size=40g $RBAC $DEV
