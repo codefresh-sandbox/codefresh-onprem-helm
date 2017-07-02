a87486f53d2bae8fa1a72b12adfb42cbc68d165f
Alexei Ledenev <alexei.led@gmail.com>
2017-07-02 12:50:37 +0300

    ingress is optional

--- start_minikube.sh
+++ start_minikube.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 # install xhive docker machine vm driver
-brew install docker-machine-driver-xhyve
+# brew install docker-machine-driver-xhyve
 
 # uncomment to enable RBAC
 # RBAC="--extra-config=apiserver.Authorization.Mode=RBAC"
 
 # uncomment to enable development on localhost
-# DEV="--network-plugin=kubenet --extra-config=kubelet.PodCIDR=10.10.0.0/24 --extra-config=kubelet.NonMasqueradeCIDR=10.10.0.0/24"
+DEV="--network-plugin=kubenet --extra-config=kubelet.PodCIDR=10.10.0.0/24 --extra-config=kubelet.NonMasqueradeCIDR=10.10.0.0/24"
 
-minikube start --cpus=4 --memory=8192 --vm-driver=xhyve --disk-size=40g $RBAC $DEV
+# uncoment to use v1.7.0
+KUBE_VER=v1.7.0
+# KUBE_VER=v1.6.4
+
+# VM=xhyve
+VM=virtualbox
+
+minikube start --cpus=4 --memory=8192 --vm-driver=$VM --disk-size=40g --kubernetes-version=$KUBE_VER $RBAC $DEV
