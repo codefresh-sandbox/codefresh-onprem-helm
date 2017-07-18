cad1a34f8a48b814e79154fae52181d5adaf820b
Alexei Ledenev <alexei.led@gmail.com>
2017-07-18 11:46:04 +0300

    minikube start

--- start_minikube.sh
+++ start_minikube.sh
@@ -8,6 +8,11 @@ minikube profile ${PROFILE}
 # uncomment to enable development on localhost
 DEV=${DEV:-"--network-plugin=kubenet --extra-config=kubelet.PodCIDR=10.10.0.0/24 --extra-config=kubelet.NonMasqueradeCIDR=10.10.0.0/24"}
 
+# ISO_URL=${ISO_URL:-"--iso-url https://storage.googleapis.com/minikube-builds/1658/minikube-testing.iso"}
+ISO_URL=${ISO_URL:-""}
+
+DOCKER_OPTS=${DOCKER_OPTS:-"--docker-opt storage-driver=overlay2"}
+
 # others: v1.6.4
 KUBE_VER=${KUBE_VER:-v1.7.0}
 
@@ -30,7 +35,7 @@ MEMORY=${MEMORY:-4096}
 # Docker registry mirror: IP for VirtualBox
 MIRROR=${MIRROR:-"--registry-mirror=http://localhost:15000 --mount --mount-string=$HOME/.minikube/mirror:/mirror"}
 
-minikube start --cpus=${CPUS} --memory=${MEMORY} --vm-driver=${VM_DRV} --disk-size=${DISK_SIZE} --kubernetes-version=${KUBE_VER} ${MIRROR} ${RBAC} ${DEV}
+minikube start --cpus=${CPUS} --memory=${MEMORY} --vm-driver=${VM_DRV} --disk-size=${DISK_SIZE} --kubernetes-version=${KUBE_VER} ${MIRROR} ${RBAC} ${DEV} ${ISO_URL} ${DOCKER_OPTS}
 
 # run Registry mirror mounted to local folder inside minikube
 if [[ ! -z "${MIRROR}" ]]; then
