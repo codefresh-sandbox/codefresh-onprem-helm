fbb210bf656f1d4e707138038dee6a235a2daa7d
Alexei Ledenev <alexei.led@gmail.com>
2017-07-18 11:17:03 +0300

    customize minikube start script

--- start_minikube.sh
+++ start_minikube.sh
@@ -4,29 +4,29 @@
 # RBAC="--extra-config=apiserver.Authorization.Mode=RBAC"
 
 # uncomment to enable development on localhost
-${DEV:="--network-plugin=kubenet --extra-config=kubelet.PodCIDR=10.10.0.0/24 --extra-config=kubelet.NonMasqueradeCIDR=10.10.0.0/24"}
+${DEV:-"--network-plugin=kubenet --extra-config=kubelet.PodCIDR=10.10.0.0/24 --extra-config=kubelet.NonMasqueradeCIDR=10.10.0.0/24"}
 
 # others: v1.6.4
-${KUBE_VER:=v1.7.0}
+${KUBE_VER:-v1.7.0}
 
 # others: xhyve
-${VM_DRV:=virtualbox}
+${VM_DRV:-virtualbox}
 # install xhive docker machine vm driver, if needed
 if [[ "$VM_DRV" == "xhyve" ]]; then
   brew install docker-machine-driver-xhyve
 fi
 
 # Storage size
-${DISK_SIZE:=20g}
+${DISK_SIZE:-20g}
 
 # CPUS to use
-${CPUS:=2}
+${CPUS:-2}
 
 # RAM to use
-${MEMORY:=4096}
+${MEMORY:-4096}
 
 # Docker registry mirror: IP for VirtualBox
-${MIRROR:="--registry-mirror=http://localhost:15000 --mount --mount-string=$HOME/.minikube/mirror:/mirror"}
+${MIRROR:-"--registry-mirror=http://localhost:15000 --mount --mount-string=$HOME/.minikube/mirror:/mirror"}
 
 minikube start --cpus=${CPUS} --memory=${MEMORY} --vm-driver=${VM_DRV} --disk-size=${DISK_SIZE} --kubernetes-version=${KUBE_VER} ${MIRROR} ${RBAC} ${DEV}
 
